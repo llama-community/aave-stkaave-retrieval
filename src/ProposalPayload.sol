6dac10878614984a1baa93dc3980164728f50812
Rajath Alex <rajathalex@gmail.com>
2022-09-20 12:53:34 -0400

    Initial commit

---
+++ src/ProposalPayload.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+/**
+ * @title <TITLE>
+ * @author <AUTHOR>
+ * @notice <DESCRIPTION>
+ * Governance Forum Post:
+ * Snapshot:
+ */
+contract ProposalPayload {
+    /// @notice The AAVE governance executor calls this function to implement the proposal.
+    function execute() external {}
+}
